df90910fc2fb857318529676c82b9532e520cb5e
zeinab <swarsama@hotmail.co.uk>
2024-01-29 18:51:40 +0000

    added card componenets

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,9 @@
 function Card () {
   return (
     <div className="card">
-      <img src=""></img>
-      <h2></h2>
-      <p></p>
-
+      <img className="card-image" alt="Apple pie picture" src="https://natashaskitchen.com/wp-content/uploads/2019/10/Apple-Pie-2.jpg" width="200" height="200"></img>
+      <h2 className="card-title">Apple Pie</h2>
+      <p className="card-text">Best apple pie in the world</p>
   </div>
   );
 
