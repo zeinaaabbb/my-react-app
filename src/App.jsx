b09f5bc86f416a7e14de8f453ac92f79418ce781
zeinab <swarsama@hotmail.co.uk>
2024-01-29 19:06:41 +0000

    created button using external styling with CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Food from "./Food.jsx";
 import Card from "./Card.jsx";
+import Button from "./Button.jsx"
 
 function App() {
   return(
     <>
       <Header></Header>
+      <Button></Button>
       <Food></Food>
       <Card></Card>
       <Card></Card>
