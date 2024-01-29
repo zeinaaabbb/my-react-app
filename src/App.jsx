68f6e9621356761175f9c3d71b46a26fb3ccfce2
zeinab <swarsama@hotmail.co.uk>
2024-01-29 22:33:10 +0000

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Food from "./Food.jsx";
 import Card from "./Card.jsx";
 import Button from "./Button/Button.jsx"
 import Student from "./Student.jsx"
+import UserGreeting from "./UserGreeting.jsx";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <>
       <Header></Header>
       <Button></Button>
+      <UserGreeting isLoggedIn={true} ></UserGreeting>
       <Student name ="Spongebob" age={30} isStudent={true}></Student>
       <Student name="Patrick" age={42} isStudent={false}></Student>
       <Student name="Squidward" age={50} isStudent={false}></Student>
