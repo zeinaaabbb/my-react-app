beb75d709f345128b1c4aba8da8214d4e0c98dcb
zeinab <swarsama@hotmail.co.uk>
2024-01-29 19:26:06 +0000

    button created through module

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Food from "./Food.jsx";
 import Card from "./Card.jsx";
-import Button from "./Button.jsx"
+import Button from "./Button/Button.jsx"
 
 function App() {
   return(
--- src/Button.jsx
+++
@@ -1,7 +0,0 @@
-function Button() {
-  return (
-    <button className="button">Click me</button>
-  );
-}
-
-export default Button
