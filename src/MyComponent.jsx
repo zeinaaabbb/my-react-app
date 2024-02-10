4e7861b2f46af4df3d52c333b7cd9307968af947
zeinab <swarsama@hotmail.co.uk>
2024-02-10 07:23:46 +0000

    isemployed usestate toggle

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -4,6 +4,7 @@ function MyComponent() {
 
   const [name, setName] = useState();
   const [age, setAge] = useState(0);
+  const [isEmployed, setIsEmployed] = useState(false);
 
   //Use setter function to change name/value on the virtual DOM
   //will trigger a render of virtual DOM normal variables dont
@@ -16,12 +17,19 @@ function MyComponent() {
     setAge(age +1);
   }
 
+  const toggleEmployedStatus = () => {
+    setIsEmployed(!isEmployed);
+  }
+
   return(<div>
     <p>name : {name} </p>
     <button onClick={updateName}>Set Name</button>
 
     <p>age : {age} </p>
     <button onClick={incrementAge}>Increment Age</button>
+
+    <p>isEmployed : {isEmployed ? "Yes" : "No"} </p>
+    <button onClick={toggleEmployedStatus}>Toggle Status</button>
   </div>)
 }
 
